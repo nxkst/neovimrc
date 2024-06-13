a9dc28f31fd4c5269385c50abf5d5364076dfd6e
nxkst <foo@bar.thing>
2024-06-13 19:58:09 +0300

    changed default colorscheme to folke/tokyonight.nvim

--- nvim/lua/nxkst/plugins/colors.lua
+++ nvim/lua/nxkst/plugins/colors.lua
@@ -1,6 +1,5 @@
 function ColorMyPencils(color)
 	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
@@ -9,7 +8,17 @@ end
 
 return {
 
-    { "mhartington/oceanic-next" },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            require("tokyonight").setup({})
+            ColorMyPencils()
+            vim.cmd("colorscheme tokyonight-night")
+        end
+    },
 
     {
         "rose-pine/neovim",

47bd3378d1f5e3467998a3f05877252b1ac81449
nxkst <foo@bar.thing>
2024-06-13 22:36:58 +0300

    some minor changes

--- nvim/lua/nxkst/remap.lua
+++ nvim/lua/nxkst/remap.lua
@@ -2,15 +2,20 @@ vim.g.mapleader = " "
 
 local remap = vim.keymap.set
 
-remap("v", "J", ":m '>+1<CR>gv=gv")
-remap("v", "K", ":m '<-2<CR>gv=gv")
 remap("n", "<leader>pv", vim.cmd.Ex)
+
 remap("n", "<C-u>", "<C-u>zz")
 remap("n", "<C-d>", "<C-d>zz")
+
 remap({"n", "v"}, "<leader>y", [["+y]])
 remap({"n", "v"}, "<leader>p", [["+p]])
+
 remap("n",
     "<leader>ee",
     "ifn main() -> () {<CR>}<Esc>O"
 )
+
+remap("v", "J", ":m '>+1<CR>gv=gv")
+remap("v", "K", ":m '<-2<CR>gv=gv")
+
 remap('n', '<Esc>', '<cmd>nohlsearch<CR>')

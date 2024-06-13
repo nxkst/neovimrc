9b5c58b06e472ded19d70b1b33c3a7e22f2f4125
nxkst <foo@bar.thing>
2024-06-13 20:34:32 +0300

    changed treesitter configuration

--- nvim/lua/nxkst/plugins/treesitter.lua
+++ nvim/lua/nxkst/plugins/treesitter.lua
@@ -1,36 +1,11 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter.configs").setup({
-            -- A list of parser names, or "all"
-            ensure_installed = {
-                "vimdoc", "javascript", "typescript", "c", "lua", "rust",
-                "jsdoc", "bash",
-            },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-            auto_install = true,
-
-            highlight = {
-                -- `false` will disable the whole extension
-                enable = true,
-            },
-        })
-
-        local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        treesitter_parser_config.templ = {
-            install_info = {
-                url = "https://github.com/vrischmann/tree-sitter-templ.git",
-                files = {"src/parser.c", "src/scanner.c"},
-                branch = "master",
-            },
+        require'nvim-treesitter.configs'.setup {
+            -- A list of parser names, or "all" (the five listed parsers should always be installed)
+            ensure_installed = { "c", "lua", "javascript", "typescript",
+            "python", "rust", "go", "vim", "vimdoc", "query" },
         }
-
-        vim.treesitter.language.register("templ", "templ")
     end
 }
+

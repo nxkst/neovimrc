f22e39c818593fee485b159defa1e05fd49c288d
nxkst <foo@bar.thing>
2024-06-19 18:32:40 +0300

    disabled showmode by default

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,7 +14,7 @@ vim.opt.relativenumber = true
 vim.opt.mouse = "a"
 
 -- Don't show the mode, since it's already in the status line
--- vim.opt.showmode = false
+vim.opt.showmode = false
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.

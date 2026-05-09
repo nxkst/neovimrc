a8881f4958e0b44b5eaaa387faf54a105c6f6d2c
nxkst <nxkststxp@gmail.com>
2026-05-10 00:36:02 +0300

    Added conform.nvim, also disabled notification on lazy config change

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+                c = { "astyle" },
+				cpp = { "astyle" },
+				python = { "isort", "black" },
+			},
+		})
+
+		vim.keymap.set("n", "<leader>=", '<cmd>lua require("conform").format()<CR>')
+	end,
+}

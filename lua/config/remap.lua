2698f981d7f147f97a4e5c2ded2a3f987a5c2b72
nxkst <nxkststxp@gmail.com>
2026-05-10 17:23:17 +0300

    Better find and replace remap

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -15,4 +15,4 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set("n", "<leader>fr", ":%s/")
+vim.keymap.set("n", "<leader>fr", ":%s/\\<<C-r><C-w>\\>//g<Left><Left>")
